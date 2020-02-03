ddd6633b1176bb5ba495ceb94d6245358b351017
Shreyas <44416610+ShreyasShankar@users.noreply.github.com>
2020-02-03 00:39:28 -0500

    completed prototype Net class

--- HW2/Solution/nn.py
+++ HW2/Solution/nn.py
@@ -23,7 +23,7 @@ class Net(nn.Module):
         ReLU
         Dropout
         Fully-connected
-        Softmax
+        Log Softmax
     '''
 
     def __init__(self):
@@ -44,14 +44,66 @@ class Net(nn.Module):
             # ReLU
             nn.ReLU(inplace=True),
             # Batch Normalization
-            nn.BatchNorm2d(num_features=20))
+            nn.BatchNorm2d(num_features=20),
+            # Flatten
+            nn.Flatten(),
+            # Dropout 15%
+            nn.Dropout(0.15, inplace=True))
 
         self.fc = nn.Sequential(
             # Fully-connected linear layer
-            nn.Linear(2*2*20, 100),
+            nn.Linear(2*2*20, 128),
             # ReLU
             nn.ReLU(inplace=True),
             # Dropout 50%
             nn.Dropout(inplace=True),
             # Final fully-connected linear layer
-            nn.Linear(100, 5))
+            nn.Linear(128, 5),
+            # Log Softmax
+            nn.LogSoftmax())
+
+    def forward(self, x):
+        ''' Compute feedforward '''
+        x = self.cnn(x)
+        x = self.fc(x)
+        return x
+
+    def train(self, data, loss, epoch, optimizer, device='cpu'):
+        ''' Train network with backpropagation '''
+        # Set model to training mode
+        self.train()
+        # Get inputs and send them to the appropriate device (cpu or cuda)
+        inputs = torch.from_numpy(data.x_train)
+        targets = torch.from_numpy(data.y_train)
+        inputs = inputs.to(device)
+        targets = targets.to(device)
+        # Compute model output from given inputs
+        outputs = self(inputs)
+        # Compute loss
+        obj_val = loss(self.forward(inputs), targets)
+        # Initialize gradients to 0
+        optimizer.zero_grad()
+        # Propagate loss
+        obj_val.backward()
+        # Update optimizer
+        optimizer.step()
+        # Return loss for tracking and visualizing
+        return obj_val.item()
+
+    def test(self, data, loss, epoch, device='cpu'):
+        ''' Test network on cross-validation data '''
+        # Set model to evaluation mode
+        self.eval()
+        # Deactivate autograd engine
+        with torch.no_grad():
+            # Get inputs and send them to the appropriate device (cpu or cuda)
+            inputs = torch.from_numpy(data.x_test)
+            targets = torch.from_numpy(data.y_test)
+            inputs = inputs.to(device)
+            targets = targets.to(device)
+            # Compute model output from given inputs
+            outputs = self(inputs)
+            # Compute loss
+            cross_val = loss(self.forward(inputs), targets)
+        # Return loss for tracking and visualizing
+        return cross_val.item()
