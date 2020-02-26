ae972ea00e986b8f3cf7cfed63b10de0e7789bfa
Shreyas <44416610+ShreyasShankar@users.noreply.github.com>
2020-02-25 23:44:55 -0500

    commit

--- HW3/Solution/main.py
+++ HW3/Solution/main.py
@@ -1,32 +1,45 @@
 #!/usr/bin/env python3
 
+from ising_math import *
+
 import argparse
 import torch
+import os
+import json
+import torch.nn as nn
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 
 def get_args():
     ''' Parse arguments from CLI '''
-    parser = argparse.ArgumentParser(
-        description='Multi-label CNN classifier for the set of 14x14 even numbers from MNIST')
+    parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--params', type=str, metavar='./files/param.json',
                         default='./files/param.json',
                         help='Path to .json file containing hyperparameters')
-    parser.add_argument('-d', '--data', type=str, metavar='../Problem/even_mnist.csv',
-                        default='../Problem/even_mnist.csv',
-                        help='Path to .csv file containing the 14x14 dataset')
+    parser.add_argument('-d', '--data', type=str, metavar='../Problem/in.txt',
+                        default='../Problem/in.txt',
+                        help='Path to .txt file containing the dataset')
     parser.add_argument('-r', '--results', default=False,
                         metavar='./results',
                         help='Directory where loss plot should be saved, if desired. Results will be saved to file only if path is provided')
     parser.add_argument('-v', '--verbose', type=bool, default=False,
                         help='Boolean flag for high verbosity output')
-    parser.add_argument('-c', '--cuda', type=bool, default=False,
-                        help='Boolean flag indicating whether or not to use CUDA GPU')
 
     return parser.parse_args()
 
 
 def run():
-    get_args()
+    args = get_args()
+
+    with open(args.params) as p:
+        params = json.load(p)
+
+    data = np.loadtxt(args.data, dtype=str)
+    J = np.random.choice([-1, 1], size=4)  # Random initialization of J
+    loss = nn.KLDivLoss(reduction='batchmean')
+    train_loss = []
 
 
 if __name__ == '__main__':
