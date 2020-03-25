f1f870da96d8456618e7137ba52e556b2cd34d4e
Shreyas <44416610+ShreyasShankar@users.noreply.github.com>
2020-03-25 01:19:22 -0400

    main ready to run, no final 100 image output yet

---
+++ HW4/Solution/main.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+from load_data import Data
+from vae import VAE
+
+import torch
+import json
+import argparse
+import os
+from tqdm import tqdm
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("darkgrid")
+
+
+def get_args():
+    ''' Parse arguments from CLI '''
+    parser = argparse.ArgumentParser(
+        description='Multi-label Variational Auto-encoder for the set of 14x14 even numbers from MNIST')
+    parser.add_argument('-p', '--params', type=str, metavar='./files/param.json',
+                        default='./files/param.json',
+                        help='Path to .json file containing hyperparameters')
+    parser.add_argument('-d', '--data', type=str, metavar='../Problem/even_mnist.csv',
+                        default='../Problem/even_mnist.csv',
+                        help='Path to .csv file containing the 14x14 dataset')
+    parser.add_argument('-r', '--results', default=False,
+                        metavar='./results',
+                        help='Directory where loss plot should be saved, if desired. Results will be saved to file only if path is provided')
+    parser.add_argument('-v', '--verbose', type=bool, default=False,
+                        help='Boolean flag for high verbosity output')
+    parser.add_argument('-c', '--cuda', type=bool, default=False,
+                        help='Boolean flag indicating whether or not to use CUDA GPU')
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    with open(args.params) as p:
+        params = json.load(p)
+    test_size = int(params['test_size'])
+
+    # CUDA usage
+    use_cuda = torch.cuda.is_available() and args.cuda
+    device = torch.device('cuda' if use_cuda else 'cpu')
+
+    # Initialize network and get data from .csv file
+    model = VAE().to(device)
+    data = Data(data_file=args.data, test_size=test_size)
+    model.init_data(data, device)
+
+    # Define Adam optimizer and multi-class cross entropy loss function
+    optimizer = optim.Adam(model.parameters(), lr=params['learning_rate'])
+    loss = torch.nn.CrossEntropyLoss()
+
+    # Training and evaluation cycle
+    # Initialize lists
+    obj_vals = []
+    cross_vals = []
+    epochs = int(params['num_epochs'])
+    batch_size = int(params['batch_size'])
+
+    print('Training over {} epochs:'.format(epochs))
+    with tqdm(total=epochs, dynamic_ncols=True) as pbar:
+        for epoch in range(1, epochs+1):
+            # Training
+            train_val = model.backprop(batch_size, optimizer)
+            obj_vals.append(train_val)
+
+            # Cross-validation evaluation
+            test_val = model.test(min(test_size, batch_size))
+            cross_vals.append(test_val)
+
+            # Update progress bar
+            pbar.update()
+
+            # High verbosity report
+            if args.verbose:
+                if not ((epoch) % params['display_epochs']):
+                    print('\nEpoch [{}/{}]'.format(epoch, epochs)
+                          + '\tTraining Loss: {:.4f}'.format(train_val)
+                          + '\tTest Loss: {:.4f}'.format(test_val))
+
+    # Final loss report
+    print('Final training loss: {:.4f}'.format(obj_vals[-1]))
+    print('Final test loss: {:.4f}'.format(cross_vals[-1]))
+
+    # Plot loss and save results if desired
+    plt.plot(range(1, epochs+1), obj_vals, label='Training loss', color='blue')
+    plt.plot(range(1, epochs+1), cross_vals, label='Test loss', color='green')
+    plt.legend()
+    if args.results:
+        if not os.path.exists(args.results):
+            os.mkdir(args.results)
+        plt.savefig(os.path.join(args.results, 'loss_report.png'), dpi=600)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
